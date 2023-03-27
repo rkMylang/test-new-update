64beba4bc0edf433eae29bae49dc2012ec2a772b
rkMylang <rohit@mylang.in>
2023-03-27 15:55:43 +0530

    added env variables in next.config file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     CURRENT_VERSION: process.env.CURRENT_VERSION,
-    LATEST_VERSION: process.env.LATEST_VERSION
+    LATEST_VERSION: process.env.LATEST_VERSION,
+    APP_NAME: process.env.APP_NAME,
+    PROJECT_ID: process.env.PROJECT_ID,
+    VERCEL_TOKEN: process.env.VERCEL_TOKEN,
+    VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA
   }
 }
 
