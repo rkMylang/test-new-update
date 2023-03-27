4c92a8e441e5d835c21ef905c295dc8902041f80
rkMylang <rohit@mylang.in>
2023-03-27 18:49:00 +0530

    removed github/vercel api calls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,45 +3,69 @@ import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 
 import getDeployments from "@/pages/api/get-deployments";
+// import { Octokit } from 'octokit';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    let envVariables = {
-      "curVersion": process.env.CURRENT_VERSION,
-      "latestVersion": process.env.LATEST_VERSION,
-      "appName": process.env.APP_NAME,
-      "projectId": process.env.PROJECT_ID,
-      "bearerToken": process.env.VERCEL_TOKEN,
-      "verceGitCommitSha": process.env.VERCEL_GIT_COMMIT_SHA
-    }
+  // useEffect(() => {
+  //   let envVariables = {
+  //     "curVersion": process.env.CURRENT_VERSION,
+  //     "latestVersion": process.env.LATEST_VERSION,
+  //     "appName": process.env.APP_NAME,
+  //     "projectId": process.env.PROJECT_ID,
+  //     "bearerToken": process.env.VERCEL_TOKEN,
+  //     "verceGitCommitSha": process.env.VERCEL_GIT_COMMIT_SHA
+  //   }
+    // const config = {
+    //   method: `get`,
+    //   url: `https://api.vercel.com/v6/deployments?app=${process.env.APP_NAME}&limit=5&projectId=${process.env.PROJECT_ID}&state=ready&target=production`,
+    //   headers: {
+    //     "Authorization": `Bearer ${process.env.VERCEL_TOKEN}`
+    //   }
+    // };
 
-    console.log(envVariables);
+    // fetch("https://api.vercel.com/v6/deployments", config).then((result) => {
+    //   return result.json()
+    // })
+    // .then((data) => {
+    //   console.log(data);
+    //   const isLatestVersion = envVariables.verceGitCommitSha === data?.deployments[0]?.meta?.githubCommitSha;
+    //   !isLatestVersion && alert("New Version Available! Please Reload the page...");
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
 
-    const config = {
-      method: `get`,
-      url: `https://api.vercel.com/v6/deployments?app=${process.env.APP_NAME}&limit=5&projectId=${process.env.PROJECT_ID}&state=ready&target=production`,
-      headers: {
-        "Authorization": `Bearer ${process.env.VERCEL_TOKEN}`
-      }
-    };
+    // Octokit.js
+// https://github.com/octokit/core.js#readme
+// const helperFunc = async () => {
+//   const octokit = new Octokit({
+//     auth: process.env.GITHUB_TOKEN
+//   })
+  
+//   const res = await octokit.request('GET /repos/{owner}/{repo}/deployments', {
+//     owner: 'rkMylang',
+//     repo: 'test-new-update',
+//     headers: {
+      
+//       "Access-Control-Allow-Origin": "*",
+//       'X-GitHub-Api-Version': '2022-11-28'
+//     },
+//     ref: process.env.VERCEL_GIT_COMMIT_SHA || undefined,
+//     environment: "production",
+//     per_page: 1
+//   })
 
-    fetch("https://api.vercel.com/v6/deployments", config).then((result) => {
-      return result.json()
-    })
-    .then((data) => {
-      console.log(data);
-      const isLatestVersion = envVariables.verceGitCommitSha === data?.deployments[0]?.meta?.githubCommitSha;
-      !isLatestVersion && alert("New Version Available! Please Reload the page...");
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+//   const data = res.data;
+//   console.log(data);
+//   return Promise.resolve(data);
+// }
+// console.log(helperFunc());
 
 
-    setInterval(() => {
-      console.log("Cur Git SHA: ", process.env.VERCEL_GIT_COMMIT_SHA || "sha-not-found");
-    }, 5000);
-  }, [])
+//     setInterval(() => {
+//       console.log("Cur Git SHA: ", process.env.VERCEL_GIT_COMMIT_SHA || "sha-not-found");
+//     }, 5000);
+//   }, [])
   
 
   return <Component {...pageProps} />
